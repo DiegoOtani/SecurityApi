f65f5db2d7e6e227659d8abf515af49cc1f6fdcb
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-09 00:23:22 -0300

    refactor: change how error is passed in user middleware

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -1,18 +1,6 @@
 const { body, validationResult } = require('express-validator');
 
 class BodyValidation {
-  static completeUser = () => {
-    return  [
-      body('name').isString().withMessage('Name have to be a string.'),
-      body('name').notEmpty().withMessage('Name is required.'),
-      body('email').isString().withMessage('Email have to be a string.'),
-      body('email').isEmail().withMessage('Invalid email.'),
-      body('email').notEmpty().withMessage('Email is required.'),
-      body('phone').isString().withMessage('Phone have to be a string.'),
-      body('phone').notEmpty().withMessage('Phone is required.'),
-      ...this.login(),
-    ];
-  }
 
   static loginUser = () => {
     return [
@@ -23,9 +11,22 @@ class BodyValidation {
     ]
   }
 
+  static completeUser = () => {
+    return  [
+      body('name').notEmpty().withMessage('Name is required.'),
+      body('name').isString().withMessage('Name have to be a string.'),
+      body('email').notEmpty().withMessage('Email is required.'),
+      body('email').isString().withMessage('Email have to be a string.'),
+      body('email').isEmail().withMessage('Invalid email.'),
+      body('phone').notEmpty().withMessage('Phone is required.'),
+      body('phone').isString().withMessage('Phone have to be a string.'),
+      ...this.loginUser(),
+    ];
+  }
+
   static validate = (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    if(!errors.isEmpty()) return res.status(400).json({ errors: errors.array().map(erro => erro.msg) });
     next();
   }
 }
