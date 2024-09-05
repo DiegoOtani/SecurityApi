076e0fab3d7affc281de6b5e660eb51955a8dcc6
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-05 16:33:17 -0300

    feat: add initial admin on connect bd | add: create admin service

--- src/services/User.js
+++ src/services/User.js
@@ -1,10 +1,15 @@
 const UserModel = require('../models/UserModel');
 
 class User {
+  static async checkIfAdminExists() {
+    const admin = await UserModel.findOne({ isAdmin: true });
+    return admin !== null; 
+  }
+
   static async userExists(username) {
     const user = await UserModel.findOne({ username: username});
     return user;
-  }
+  };
 
   static async register(body) {
     const user = await this.userExists(body.username);
@@ -12,7 +17,15 @@ class User {
 
     const userCreated = await UserModel.create(body);
     return userCreated;
-  }
+  };
+
+  static async createAdmin(body) {
+    const user = await this.userExists(body.username);
+    if(user) return { error: 'Username already registered' };
+
+    const adminCreated = await UserModel.create({ ...body, isAdmin: true });
+    return adminCreated;
+  };
 }
 
 module.exports = User;
