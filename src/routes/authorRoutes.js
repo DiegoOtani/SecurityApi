5b8037758893a4e4d6ca6adac1ca40c181a6a791
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-06 13:13:04 -0300

    feat: add route getById into authors routes | fix: resolving validation to valid mongo id

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { authorInfo, validate } = require('../middlewares/Author');
-const { getAll, create, edit, deleteAuthor } = require('../controllers/author');
+const { getAll, create, edit, deleteAuthor, getAuthorById } = require('../controllers/author');
 
 router.get('/', getAll);
+router.get('/:id', getAuthorById);
 router.post('/', authorInfo(), validate, create);
 router.put('/:id', edit);
 router.delete('/:id', deleteAuthor);
