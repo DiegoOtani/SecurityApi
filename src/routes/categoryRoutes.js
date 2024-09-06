4ad389db782f53f467f94e621f2a056385943628
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-09-06 13:45:58 -0300

    feat: add getById in category routes and add id validation in operations

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, edit, deleteCategory, getAll } = require('../controllers/category');
+const { register, edit, deleteCategory, getAll, getCategoryById } = require('../controllers/category');
 const { categoryInfo, validate } = require('../middlewares/Category');
 
 router.get('/', getAll);
+router.get('/:id', getCategoryById);
 router.post('/', categoryInfo(), validate, register);
 router.put('/:id', categoryInfo(), validate, edit);
 router.delete('/:id', deleteCategory);
